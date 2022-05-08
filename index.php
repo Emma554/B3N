561b0e9a0423cd358a84468b9e6ea5aed0ee37ee
Emma554 <tchakoutemmanuelle@yahoo.fr>
2022-05-08 23:58:15 +0100

    premier envoi

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Authentification</title>
+    <script src="assets\bootstrap-5.0.2\dist\js\bootstrap.min.js"></script>
+    <link rel="stylesheet" href="assets\bootstrap-5.0.2\dist\css\bootstrap.min.css">
+    <link rel="stylesheet" href="assets\fontawesome-free-6.1.1-web\css\all.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="assets\js\jquery.js"></script>
+</head>
+<body>
+    <h1>MN_EXAMINER,l'application en ligne pour vos examens</h1>
+     
+</body>
+</html>
+
